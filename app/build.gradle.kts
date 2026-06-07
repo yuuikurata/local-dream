2baddea09e5475b1d7f021d50750307e360066d8
yuuikurata <yuuikurata@gmail.com>
2026-06-07 17:50:51 +0900

    バージョンを本家と同じに戻す。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 //        minSdk = 31
         targetSdk = 36
         versionCode = 69
-        versionName = "2.6.2mod"
+        versionName = "2.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
